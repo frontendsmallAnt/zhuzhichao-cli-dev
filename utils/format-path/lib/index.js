be9d5c00919c95100ebe7900e61f697cf1e75429
zhuzhichao <1041832785@qq.com>
2023-05-16 03:24:16 +0800

    强制提交

--- utils/format-path/lib/index.js
+++ utils/format-path/lib/index.js
@@ -1,7 +1,14 @@
 /*
  * @FilePath: /zhuzhichao-cli-dev/utils/format-path/lib/index.js
  * @Author: zhuzhichao
- * @Date: 2023-05-16 03:15:54
+ * @Date: 2023-05-16 03:13:39
  * @LastEditors: zhuzhichao
- * @LastEditTime: 2023-05-16 03:15:56
+ * @LastEditTime: 2023-05-16 03:17:41
  */
+'use strict';
+
+module.exports = formatPath;
+
+function formatPath(p) {
+  return p;
+}
